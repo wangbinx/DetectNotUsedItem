bde2159bd46a2884aede428f99acc62b8eb7c8f8
wangbinx <binx.a.wang@intel.com>
2018-07-25 17:11:39 +0800

    20180725 Update script
    1. Modify run command
    2. Modify input dirs

--- GenerateUnreferencedPcd.py
+++ GenerateUnreferencedPcd.py
@@ -1,3 +1,6 @@
+'''
+DetectNotUsedItem
+'''
 import re
 import os
 import shutil
@@ -6,76 +9,86 @@ import argparse
 #
 # Globals for help information
 #
-__prog__        = 'GenerateUnreferencedPcd'
+__prog__        = 'DetectNotUsedItem'
 __version__     = '%s Version %s' % (__prog__, '0.1 ')
-__copyright__   = 'Copyright (c) 2016 - 2018, Intel Corporation. All rights reserved.'
-__description__ = "Generate all Unreferenced Pcd in Dec File, echo the DEC file, the pcd name and it's line number.\n"
+__copyright__   = 'Copyright (c) 2018, Intel Corporation. All rights reserved.'
+__description__ = "Detect unreferenced PCD and GUID/Protocols/PPIs.\n"
 
-class PROCESS(object):
+class Common(object):
 
-	def __init__(self,DecPathDict):
-		self.Dec = list(DecPathDict.keys())[0]
-		self.Path = DecPathDict[self.Dec]
-		#Save output information to self.Log
-		self.Log = []
+	def FindFileByExtendName(self, ExtendName, Path):
+		Files = []
+		for path in Path:
+			for root, dirs, files in os.walk(path, topdown=True, followlinks=False):
+				for name in files:
+					if os.path.splitext(name)[-1].lower() == '.%s' % ExtendName:
+						FullPath = os.path.join(root, name)
+						Files.append(FullPath)
+		return Files
 
-	#Parse Dec and Inf file to get Line number and Pcd Name
-	#return section name, the Pcdname and comments with there line number
+	# Parse Dec and Inf file to get Line number and Pcd Name
+	# return section name, the Pcdname and comments with there line number
 	def ParseContent(self,File):
 		SectionRE = re.compile(r'\[(.*)\]')
 		ParseFlag = False
 		Comments ={}
-		comment = []
+		comment_num = []
 		SectionName = {}
-		PcdName = {}
-		ParsedSection = ["LibraryClasses", "Guids", "Ppis", "Protocols"]
+		Name = {}
 		with open(File, 'r') as F:
 			for num, value in enumerate(F):
-				name = SectionRE.findall(value)
-				if name:
-					if (name[0] in ParsedSection) or "Pcd" in name[0] or "Guids" in name[0]:
-						SectionName[num] = name[0]
-						ParseFlag = True
-						continue
-					else:
-						ParseFlag = False
+				Section = SectionRE.findall(value)
+				if Section and not (value.strip().startswith("#")):
+					section_name = Section[0]
+					ParseFlag= self._InFlag(section_name)
+					SectionName[num] = section_name
+					continue
 				if ParseFlag == True:
 					if value.strip().startswith("#"):
 						CommentsFlag = True
 					else:
 						CommentsFlag = False
 					if CommentsFlag:
-						comment.append(num)
+						comment_num.append(num)
 					if not (value.strip().startswith("#")):
-						comment.append(num)
+						comment_num.append(num)
 						if not value == "\n":
-							pcd = self._split(value)
-							PcdName[num] = pcd
-							Comments[pcd] = comment
-							comment = []
-		return SectionName, PcdName,Comments
+							name = self._split(value)
+							Name[num] = name
+							Comments[name] = comment_num
+							comment_num = []
+		return SectionName, Name,Comments
 
 	#Split the statement in Dec and Inf to get the PcdName
 	def _split(self,value):
 		return value.replace(' ','').split('=')[0].split('|')[0].split('#')[0].strip()
 
-	def FindFileByExtendName(self,ExtendName,Path):
-		Files = []
-		for root, dirs, files in os.walk(Path, topdown=True, followlinks=False):
-			for name in files:
-				if os.path.splitext(name)[-1].lower() == '.%s'%ExtendName:
-					FullPath = os.path.join(root,name)
-					Files.append(FullPath)
-		return Files
+	def _InFlag(self,name):
+		ParseFlag = False
+	#	section =''
+		ParsedSection = ["LibraryClasses", "Guids", "Ppis", "Protocols", "Pcd"]
+		for section_keyword in ParsedSection:
+			if section_keyword in name:
+				ParseFlag = True
+	#			section = name
+		return ParseFlag
 
-	#Find all Inf file in the path to generate the pcd name
-	def FindInf(self):
-		InfList=self.FindFileByExtendName("inf",self.Path)
-		return InfList
+class PROCESS(Common):
+
+	def __init__(self,DecPath,InfDirs):
+		self.Dec = DecPath
+		self.InfPath = InfDirs
+		#Save output information to self.Log
+		self.Log = []
 
 	def ParseDec(self):
 		Section, Pcd,Comments = self.ParseContent(self.Dec)
 		return Section, Pcd, Comments
+
+	#Find all Inf file in dirs to generate name
+	def FindInf(self):
+		InfList=self.FindFileByExtendName("inf",self.InfPath)
+		return InfList
 
 	def ParseInfFile(self):
 		INF_Dict ={}
@@ -84,31 +97,43 @@ class PROCESS(object):
 			INF_Dict[inf] = tmp_Section,tmp_Pcd
 		return INF_Dict
 
-	#Compare the Pcd name in Dec and Pcd name in Inf, if not equal, save the
+	#Compare the LibraryClass/Guid/PcdCname in Dec and LibraryClass/Guid/PcdCname in Inf, if not equal, save the
 	#Pcd name, the Line number, and it's comments
 	def CheckPcd(self):
 		unuse ={}
 		DecSection, DecPcdName,DecComments = self.ParseDec()
 		InfsDict = self.ParseInfFile()
-		print("DEC File:%s"%self.Dec)
-		print("Line Number     Unused PcdName")
-		self.Log.append("DEC File:%s\nLine Number     Unused PcdName\n"%self.Dec)
+		print("DEC File:\n%s"%self.Dec)
+		print("Line Number%sUnused Item"%(" "*5))
+		self.Log.append("DEC File:\n%s\nLine Number%sUnused PcdName\n"%(self.Dec,(" "*5)))
 		for LineNum in list(DecPcdName.keys()):
-			DPName = DecPcdName[LineNum]
+			DecName = DecPcdName[LineNum]
 			MatchFlag = False
 			for Inf in InfsDict:
 				Inf_dict = InfsDict[Inf]
 				Inf_section_dict, InfPcdName_dict = Inf_dict
 				for key in InfPcdName_dict.keys():
-					InfPcdName = InfPcdName_dict[key]
-					if DPName in InfPcdName:
+					InfName = InfPcdName_dict[key]
+					if (DecName == InfName) or (DecName == InfName.split('.',1)[0]):
 						MatchFlag = True
 			if MatchFlag == False:
-				unuse[LineNum] = DPName
-				print("%s%s%s"%("{:>6}".format(LineNum+1)," "*12,DPName))
-				self.Log.append("%s%s%s\n"%("{:>6}".format(LineNum+1)," "*12,DPName))
+				unuse[LineNum] = DecName
+				print("%s%s%s"%("{:>6}".format(LineNum+1)," "*12,DecName))
+				self.Log.append("%s%s%s\n"%("{:>6}".format(LineNum+1)," "*12,DecName))
 		self.Log.append("\n")
+		self.AppendSectionInfoToLog(DecSection,unuse)
 		return unuse,DecComments
+
+	def AppendSectionInfoToLog(self,DecSection,UnuseDict):
+		pass
+		for Name_num in list(UnuseDict.keys()):
+			section_list = list(sorted(DecSection.keys()))
+			for Section_num in section_list:
+				if Name_num < Section_num:
+					Section = DecSection[section_list[section_list.index(Section_num)-1]]
+					break
+		pass
+
 
 	#Clean the Pcd from Dec file which not used in Inf file.
 	#The origin Dec file will rename to DecFile.bak
@@ -121,63 +146,55 @@ class PROCESS(object):
 		with open(self.Dec, 'r') as Dec:
 			lines = Dec.readlines()
 		shutil.copyfile(self.Dec,self.Dec+'.bak')
-		with open(self.Dec, 'w+') as T:
-			for linenum in range(len(lines)):
-				if linenum in removednum:
-					continue
-				else:
-					T.write(lines[linenum])
-		print("New Dec File Name:%s, origin Dec File Name:%s.bak"%(self.Dec,self.Dec))
+		try:
+			with open(self.Dec, 'w+') as T:
+				for linenum in range(len(lines)):
+					if linenum in removednum:
+						continue
+					else:
+						T.write(lines[linenum])
+			print("New Dec File Name:%s, origin Dec File Name:%s.bak"%(self.Dec,self.Dec))
+		except Exception as err:
+			print(err)
 
-#Function for Write log to log file.
-def WriteLog(FileName,content,Flag):
-	if Flag == True:
-		with open(FileName, 'w+') as log:
-			for line in content:
-				log.write(line)
-	else:
-		with open(FileName, 'a+') as log:
-			for line in content:
-				log.write(line)
-	print("Log file Name:%s"%FileName)
-	log.close()
+class Main(object):
 
-def mainprocess(PATH,Flag):
-	LogFileFlag = True
-	DecPathDict ={}
-	for path in PATH:
-		decfile = {}
-		for root, dirs, files in os.walk(path, topdown=True, followlinks=False):
-			for name in files:
-				if os.path.splitext(name)[-1].lower() == ".dec":
-					file = os.path.join(root, name)
-					DecPathDict[file] = root
-					decfile[file] = root
-		if decfile == {}:
-			print("ERROR: No Dec File Found in path %s"%path)
-		for i in list(decfile.keys()):
-			tmp ={}
-			tmp[i] =decfile[i]
-			R = PROCESS(tmp)
-			unuse,comment = R.CheckPcd()
-			WriteLog("Log.txt",R.Log,LogFileFlag)
-			LogFileFlag = False
-			if Flag:
-				R.Clean(unuse,comment)
+	def mainprocess(self,Dec, Dirs, CleanFlag, LogPath):
+		R = PROCESS(Dec,Dirs)
+		unuse, comment = R.CheckPcd()
+		self.WriteLog(R.Log, LogPath)
+		if CleanFlag:
+			R.Clean(unuse, comment)
+
+	# Function for Write log to log file.
+	def WriteLog(self,content, FileName):
+		if FileName != False:
+			try:
+				with open(FileName, 'w+') as log:
+					for line in content:
+						log.write(line)
+				print("Log save to file:%s" %FileName)
+				log.close()
+			except Exception as e:
+				print("Save log Error:%s"%e)
 
 def main():
 	parser = argparse.ArgumentParser(prog=__prog__,
-		                                 description=__description__ + __copyright__,
-		                                 conflict_handler='resolve')
-	parser.add_argument('-v', '--version', action='version', version=__version__,
-		                    help="show program's version number and exit")
-	parser.add_argument('-i', '--input',metavar="", action = 'append', dest='input', help="Input the package path,e.g. C:\\MyWorkSpace\\Nt32Pkg")
-	parser.add_argument('--clean', action = 'store_true', default=False, dest='clean', help="Clean the Unreferenced PCD from DEC file")
+		                            description=__description__ + __copyright__,
+		                            conflict_handler='resolve')
+	parser.add_argument('-i', '--input', metavar="", dest='dec',help="Input DEC file path.")
+	parser.add_argument('--dirs', metavar="", action='append', dest='dirs', help="Input dir/dirs which include the INF file.")
+	parser.add_argument('--clean', action = 'store_true', default=False, dest='clean', help="Clean the unreferenced PCD from DEC file.")
+	parser.add_argument('--log', metavar="", dest="log", default=False,help="Export log to file")
 	options = parser.parse_args()
-	if options.input:
-		mainprocess(options.input,options.clean)
+	if options.dec:
+		if options.dirs:
+			M = Main()
+			M.mainprocess(options.dec,options.dirs,options.clean,options.log)
+		else:
+			print("Error: the following argument is required:'--dirs', please see '-h' for help")
 	else:
-		print("Error: the following argument is required:'-i'")
+		print("Error: the following argument is required:'-i', please see '-h' for help")
 
 if __name__ == '__main__':
 	main()
